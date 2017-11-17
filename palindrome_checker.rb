877f0f43c0178c85fd80bafcb05772ffc6c4b694
Ariel Burriss <ariburr@gmail.com>
2017-11-17 10:20:01 -0700

    cleanup

--- palindrome_checker.rb
+++ palindrome_checker.rb
@@ -1,23 +1,29 @@
 def palindrome()
-  puts "Please enter word to create a palindrome:"
-  word = gets.chomp.downcase
 
-  word_arr = word.split("")
-  cleaned_up_arr = word_arr.delete_if { |x| x == (" ") }
-  checker = cleaned_up_arr.join("")
-  reverse_arr = []
+  while true
 
-  reverse_arr.push(cleaned_up_arr.pop) until cleaned_up_arr.length === 0
+    puts "Please enter word to create a palindrome:"
+    word = gets.chomp.downcase
 
-  new_word = reverse_arr.join("")
+    word_arr = word.gsub(" ", "").split("")
+    checker = word_arr.join("")
+    reverse_arr = []
 
-  if new_word == checker
-    puts "You made a palindrome! Congrats!"
-  else
-    puts "Not a palindrome! Sorry!"
-    exit
+    reverse_arr.push(word_arr.pop) until word_arr.length === 0
+
+    new_word = reverse_arr.join("")
+
+    if new_word == checker
+      puts "You made a palindrome! Congrats!"
+    else
+      puts "Not a palindrome! Sorry!"
+      palindrome
+    end
+
   end
+
+  exit
 
 end
 
-palindrome()
+palindrome
